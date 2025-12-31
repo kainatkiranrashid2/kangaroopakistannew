f7ddd39ae9a4ff2f5506debf46f35fc0578278a2
kainat <kainat@lcisystems.com>
2025-12-31 17:50:38 +0500

    `Update About page: change pillar section padding from py-16 to py-32`

--- pages/About.tsx
+++ pages/About.tsx
@@ -98,7 +98,7 @@ const About: React.FC = () => {
       </section>
 
       {/* 2. Pillars Section: Interactive Content */}
-      <section className="py-16 -mt-16 relative z-20">
+      <section className="py-32 -mt-16 relative z-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             {pillars.map((pillar) => (
